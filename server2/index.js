fe90cb7db805868edce05ffc60f74fe5486b4a88
Khankisiyevravan <revan.xankisiyev.02@mail.ru>
2025-08-28 17:30:34 +0400

    update

--- server2/index.js
+++ server2/index.js
@@ -162,11 +162,7 @@ const createAdminUser = async () => {
 
 // createAdminUser();
 
-<<<<<<< HEAD
 const PORT = process.env.PORT || 5001;
-=======
-const PORT = process.env.PORT || 5000;
->>>>>>> origin/master
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
